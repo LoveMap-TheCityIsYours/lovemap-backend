bb9aeee4320084670eaa267203dc81b989050d79
Attila Palfi <attila.palfi.92@gmail.com>
2022-04-28 22:15:12 +0200

    smackspot api endpoint

--- src/main/kotlin/com/smackmap/smackmapbackend/smackspot/SmackSpotController.kt
+++ src/main/kotlin/com/smackmap/smackmapbackend/smackspot/SmackSpotController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/smack/spot")
+@RequestMapping("/smackspot")
 class SmackSpotController(
     private val smackSpotService: SmackSpotService,
     private val smackSpotReviewService: SmackSpotReviewService
